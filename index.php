cdd96d399b172ff9fa653b1342914486e870a7c9
Alba De biase <debiasealba@gmail.com>
2020-12-16 15:25:35 +0100

    inizializzazione laravel mix

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- Per prima cosa, strutturiamo un array che contiene tutte le domande e le relative risposte.
+Utilizzando PHP stampiamo la lista delle FAQ all'interno della pagina html.
+Una volta che siamo riusciti ad ottenere un buon risultato con array e html+php ci dedichiamo alla grafica: utilizzando Sass,
+stiliamo header e footer e il contenuto della pagina. -->
+
+<?php
+
+ ?>
